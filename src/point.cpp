ff699fdb4b09f0a0cb8cc5052e0ea3184bc36827
sacharceleur <sachasalmeron78@gmail.com>
2026-05-06 16:28:04 +0200

    Update point.cpp

--- src/point.cpp
+++ src/point.cpp
@@ -1,3 +1,7 @@
 #include "point.hpp"
-
+#include <cmath>
 Point::Point(double x_val, double y_val) : x(x_val), y(y_val) {}
+
+double Point:: distance(Point B){
+	return sqrt(pow(B.x-x,2)+pow(B.y-y,2));
+}
